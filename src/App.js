1adbc99821dd3c9f8ff1867805805dee42fc1a2b
Max Cheung <maxcheung07@gmail.com>
2023-09-20 15:09:57 -0700

    cleaned up app directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
